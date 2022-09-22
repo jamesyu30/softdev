55d629d57293520a6e063c15a6a3927c98d36e50
James Yu <jyu30@stuy.edu>
2022-09-22 13:20:24 -0400

    worked on krewes

--- 04_choose/krewes.py
+++ 04_choose/krewes.py
@@ -1,1 +1,21 @@
-"""asds"""
+import random
+"""
+James YU
+SoftDev
+k<nn>--<Title>
+2022 9 22
+Time Spent:
+
+DISCO: Don't use existing function names like random
+QCC:
+OPS SUMMARY:
+"""
+
+krewes = {2:["awong30","bchen30","cchen33"],7:["afang30","dchen30","gpark30"],8:["ali34","bwang30","eli30"]}
+
+def randomK ():
+    randKey = random.randrange(0,3,1)
+    print(randKey)
+    return krewes[randKey]
+
+print(randomK())
