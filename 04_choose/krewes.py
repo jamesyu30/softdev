b45da5663ae4d374e2abf82fd07e87bb7b23e7dd
James Yu <jyu30@stuy.edu>
2022-09-23 13:08:32 -0400

    completed random devo

--- 04_choose/krewes.py
+++ 04_choose/krewes.py
@@ -1,22 +1,35 @@
 import random
 """
-James YU
+James Yu
 SoftDev
-k<nn>--<Title>
-2022 9 22
+k04--randomDict - randomly picks a value from a dictionary
+2022-9-22
 Time Spent:
 
 DISCO: Don't use existing function names like random
        random.choice picks randomly from a list, string, or tuple
        list() typecasts to a list
 QCC:
-OPS SUMMARY:
+
+OPS SUMMARY: krewes.keys() returns the keys in krewes.
+random.choice randomly selects one of the keys.
+l is the length of the selected key.
+Then, a random index is selected based on l.
+It returns a random devo.
+
 """
 
-krewes = {2:["awong30","bchen30","cchen33"],7:["afang30","dchen30","gpark30"],8:["ali34","bwang30","eli30"]}
-#choose between 2,7,8
-def randomK ():
-    randKey = random.choice(list(krewes.keys()))
-    return krewes[randKey]
+krewes = {
+           2:["NICHOLAS",  "ANTHONY",  "BRIAN",  "SAMUEL",  "JULIA",  "YUSHA",  "CORINA",  "CRAIG",  "FANG MIN",  "JEFF",  "KONSTANTIN",  "AARON",  "VIVIAN",  "AYMAN",  "TALIA",  "FAIZA",  "ZIYING",  "YUK KWAN",  "DANIEL",  "WEICHEN",  "MAYA",  "ELIZABETH",  "ANDREW",  "VANSH",  "JONATHAN",  "ABID",  "WILLIAM",  "HUI",  "ANSON",  "KEVIN",  "DANIEL",  "IVAN",  "JASMINE",  "JEFFREY", "Ruiwen"], 
+           7:["DIANA",  "DAVID",  "SAM",  "PRATTAY",  "ANNA",  "JING YI",  "ADEN",  "EMERSON",  "RUSSELL",  "JACOB",  "WILLIAM",  "NADA",  "SAMANTHA",  "IAN",  "MARC",  "ANJINI",  "JEREMY",  "LAUREN",  "KEVIN",  "RAVINDRA",  "SADI",  "EMILY",  "GITAE",  "MAY",  "MAHIR",  "VIVIAN",  "GABRIEL",  "BRIANNA",  "JUN HONG",  "JOSEPH",  "MATTHEW",  "JAMES",  "THOMAS",  "NICOLE",  "Karen"],
+           8:["ALEKSANDRA",  "NAKIB",  "AMEER",  "HENRY",  "DONALD",  "YAT LONG",  "SEBASTIAN",  "DAVID",  "YUKI",  "SHAFIUL",  "DANIEL",  "SELENA",  "JOSEPH",  "SHINJI",  "RYAN",  "APRIL",  "ERICA",  "JIAN HONG",  "VERIT",  "JOSHUA",  "WILSON",  "AAHAN",  "GORDON",  "JUSTIN",  "MAYA",  "FAIYAZ",  "SHREYA",  "ERIC",  "JEFFERY",  "BRIAN",  "KEVIN",  "SAMSON",  "BRIAN",  "HARRY",  "Wanying", "Kevin"]
+         }
 
-print(randomK())
+#choose between 2,7,8
+#Solution 1
+def randomDevo ():
+    randKey = random.choice(list(krewes.keys()))
+    l = len(krewes[randKey])
+    return krewes[randKey][random.randrange(0,l,1)]
+
+print(randomDevo())
