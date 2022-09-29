03122def6968a784c48b3174c25013b88c38bb4a
James Yu <jyu30@stuy.edu>
2022-09-29 13:20:58 -0400

    worked on bitstream

---
+++ 05_bitstream/krewes.py
@@ -0,0 +1,19 @@
+"""
+James Yu
+SoftDev
+k05--
+2022-9-29
+Time Spent:
+
+DISCO: 
+QCC:
+OPS SUMMARY: 
+"""
+
+#def readK():
+f = open("krewes.txt","r")
+t = f.read().strip().split("@@@")
+
+print(t[0].split("$$$"))
+
+
