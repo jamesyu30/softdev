8a27e02eb77cd057b912e56a9460f630580c93a4
jamesyu <78173139+jamesyu30@users.noreply.github.com>
2022-10-06 15:45:56 -0400

    v2 done

--- 08_serve/v2/app.py
+++ 08_serve/v2/app.py
@@ -1,4 +1,4 @@
-# Clyde 'Thluffy' Sinclair
+# Nada, James, Sadi of Soup Sharks
 # SoftDev
 # Oct 2022
 
@@ -8,8 +8,7 @@ app = Flask(__name__) #create instance of class Flask
 @app.route("/")       #assign fxn to route
 def hello_world():
     print("about to print __name__...")
-    print(__name__)   #where will this go?
+    print(__name__)   #Pred: Both this and the above print statement will appear in the terminal; Pred was right
     return "No hablo queso!"
 
 app.run()
-
