a6d8d1b422b333580dd9dae7e1f91924feddad2b
jamesyu <78173139+jamesyu30@users.noreply.github.com>
2022-10-06 15:47:50 -0400

    v4 done

--- 08_serve/v4/app.py
+++ 08_serve/v4/app.py
@@ -1,4 +1,4 @@
-# Clyde 'Thluffy' Sinclair
+# James, Nada, Sadi of Soup Sharks
 # SoftDev
 # Oct 2022
 
@@ -9,8 +9,17 @@ app = Flask(__name__) #create instance of class Flask
 def hello_world():
     print("the __name__ of this module is... ")
     print(__name__)
-    return "No hablo queso!"
+    return "No hablaron queso! <br> Tenemos hambre."
 
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
     app.run()
+"""
+Predictions:
+    We know that __name__ == "__main__" so, app.debug will be true, and the run function will run.
+    
+Discos:
+So, when the code is saved, the terminal prints a whole new set.
+Furthermore, changes in the return statement can be seen upon reload.
+We also saw that the printed statements are read as html, and not formatted from python. Ie) \n does not work. <br> was needed.
+"""
