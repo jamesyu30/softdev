9d957f63d56274adb51745cea046438efe2972fc
jamesyu <78173139+jamesyu30@users.noreply.github.com>
2022-11-21 18:50:56 -0500

    api stuff

--- 20_restapi/app.py
+++ 20_restapi/app.py
@@ -9,7 +9,7 @@ def get():
         k = f.readlines() #nasa api key
     key = k[0]#changes the key from a list to a string
     data = requests.get(url=f"https://api.nasa.gov/planetary/apod?api_key={key}")#gets data from the website
-    url = json.loads(data.text)["url"] #res.text turns res into a string, json.loads converts json string to dictionary
+    url = json.loads(data.text)["url"] #data.text turns data into a string, json.loads converts json string to dictionary
     explanation = json.loads(data.text)["explanation"]
     return render_template("main.html", url=url, e=explanation)
  
