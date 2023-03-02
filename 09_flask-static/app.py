e50c3ee17d7a92cc0c5661532e98fae084e8e544
jamesyu <78173139+jamesyu30@users.noreply.github.com>
2023-03-02 13:44:37 -0500

    Update app.py

--- 09_flask-static/app.py
+++ 09_flask-static/app.py
@@ -16,4 +16,4 @@ def hello_world():
 
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
-    app.run()
+    app.run(host='0.0.0.0')
